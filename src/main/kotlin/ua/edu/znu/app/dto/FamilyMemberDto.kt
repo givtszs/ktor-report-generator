1e61084b7dda03eecfb58fca6825ad993730e59e
givtszs <givtszs@gmail.com>
2025-09-03 13:23:51 +0300

    Rename name field to fullName in the FamilyMemberDto

--- src/main/kotlin/ua/edu/znu/app/dto/FamilyMemberDto.kt
+++ src/main/kotlin/ua/edu/znu/app/dto/FamilyMemberDto.kt
@@ -1,7 +1,8 @@
 package ua.edu.znu.app.dto
 
 data class FamilyMemberDto(
+    val typeOfFamilyMember_id: String,
     val type: String,
-    val name: String,
+    val fullName: String,
     val phoneNumber: String,
 )
